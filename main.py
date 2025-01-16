dcfd4870c891171125a762c3f96b28b27c294bf6
Hana Koubova <koubovahan@gmail.com>
2025-01-16 17:24:50 +0100

    sitemap update

--- main.py
+++ main.py
@@ -371,15 +371,10 @@ def users():
 def robots():
     return send_from_directory(app.static_folder, 'robots.txt')
 
-#@app.route('/sitemap.xml')
-#def sitemap():
-#    return send_from_directory(app.static_folder, 'sitemap.xml')
-
 @app.route('/sitemap.xml')
 def sitemap():
-    return render_template('sitemap.xml')
+    return send_from_directory(app.static_folder, 'sitemap.xml')
 
     
-
 if __name__ == "__main__":
     app.run(debug=True)
